801cdba3b178928a653233dce6860568c41a0d83
Shel Soloa <a.sheldon.sol@gmail.com>
2019-03-20 18:33:40 -0400

    Updated fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,18 +3,26 @@ import Typography from "typography"
 const typography = new Typography({
 	baseFontSize: "18px",
 	baseLineHeight: 1.666,
+	headerWeight: '700',
   	headerFontFamily: [
-	    "Avenir Next",
-	    "Helvetica Neue",
-	    "Segoe UI",
-	    "Helvetica",
-	    "Arial",
-	    "sans-serif",
+  		"Inconsolata",
+	    "Georgia",
+	    "serif"
 	],
   	bodyFontFamily: [
-  		"Georgia", 
-  		"serif"
+  		"Inconsolata", 
+  		"monospace"
   	],
+  	googleFonts: [
+  		{
+  			name: "Damion",
+  			styles: ["400"]
+  		},
+  		{
+  			name: "Inconsolata",
+  			styles: ["400", "700"]
+  		}
+  	]
 })
 
 export default typography
