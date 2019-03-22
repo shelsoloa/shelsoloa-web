a75262b4256d1a2398eb6fc0f425d897af57b484
Shel Soloa <a.sheldon.sol@gmail.com>
2019-03-21 22:16:07 -0400

    Added tiles to project links

--- src/components/project-link.js
+++ src/components/project-link.js
@@ -4,7 +4,16 @@ import { Link } from "gatsby"
 import Hexagon from "./hexagon"
 
 export default props => (
-	<Link to={props.to} style={{ display:'block' }}>
-		<Hexagon background={props.background} size='20'/>
-	</Link>
+	<div>
+		
+		<Link to={props.to} style={{ display:'block'}}>
+			<Hexagon background={props.background} size='20'/>
+		</Link>
+		<Link to={props.to}>
+			<h2 style={{ 'text-align': 'center', padding: 0, 'margin': 0 }}>
+				{props.label}
+			</h2>
+		</Link>
+		
+	</div>
 )
