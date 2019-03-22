8ee6596bd551dfd673e1ba55fa5c5a1e33621bd5
Shel Soloa <a.sheldon.sol@gmail.com>
2019-03-21 22:26:15 -0400

    Cleaned up dividers

--- src/components/divider.js
+++ src/components/divider.js
@@ -3,8 +3,10 @@ import React from "react"
 
 export default props => (
 	<div className='divider'>
-		<h1>{props.title}</h1>
-		<p>{props.subtitle}</p>
+		<div className='divider__header'>
+			<h1>{props.title}</h1>
+			<p>{props.subtitle}</p>
+		</div>
 		<div className='divider__content'>
 			{props.children}
 		</div>
