e8a48263f6cc77d6ac504d844a2f557e3fd5dcba
Shel Soloa <a.sheldon.sol@gmail.com>
2019-03-23 13:12:51 -0400

    Added blog post loading from filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,15 @@ module.exports = {
 	plugins: [
 		`gatsby-plugin-sass`,
 		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `src`,
+				path: `${__dirname}/src/`,
+			}
+		},
+		`gatsby-transformer-remark`,
+		`gatsby-plugin-emotion`,
+		{
 			resolve: `gatsby-plugin-typography`,
 			options: {
 				pathToConfigModule: `src/utils/typography`,
