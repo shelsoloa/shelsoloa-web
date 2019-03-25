7621d5e085b8a24148e9c872939f0d9b0c56f365
Shel Soloa <a.sheldon.sol@gmail.com>
2019-03-25 08:52:36 -0400

    Added blog entries to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
 import '../styles/blog.scss'
 
-export default () => (
+export default ({ data }) => (
 	<Layout class='blog-page' header='Blog' subheader='~/blog/'>
 
 		<Link to='/'>Home</Link>
 		<Link to='/projects'>Projects</Link>
 
-		<p>Blog entries</p>
+		<h1>Blog Entries</h1>
+
+		{data.allMarkdownRemark.edges.map(({ node }) => (
+			<div key={node.id}>
+				<Link to={node.fields.slug}>
+					<h3>{node.frontmatter.title} -- {node.frontmatter.date}</h3>
+					<p>{node.excerpt}</p>
+				</Link>
+			</div>
+		))}
 	</Layout>
 )
+
+export const query = graphql`
+	{
+		allMarkdownRemark {
+			edges {
+				node {
+					excerpt
+					fields {
+						slug
+					}
+					frontmatter {
+						title
+						date
+					}
+				}
+			}
+		}
+	}
+`
