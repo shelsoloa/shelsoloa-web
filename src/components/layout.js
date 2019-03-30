37f935ae1be94775a05c08043d56d146839f4cab
Shel Soloa <a.sheldon.sol@gmail.com>
2019-03-29 20:04:25 -0400

    Changed site layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,19 +9,26 @@ import { HexBackground } from "./hexagon"
 
 export default props => (
 	<PageTransition>
-		<div class={props.class}>
+		<div class={'page ' + props.class}>
 			<HexBackground rows={15} columns={20}/>
+
+			<div class='sidebar'>
+				<nav>
+					<img class='pic' src="https://placeimg.com/300/300/any"/>
+
+						{/* Navigation Links */}
+						<ul class='navigation-links'>
+							<li><Link to='/'>Home</Link></li>
+							<li><Link to='/projects'>Projects</Link></li>
+							<li><Link to='/blog'>Blog</Link></li>
+						</ul>
+				</nav>
+			</div>
 
 			<content>
 				<div class='content__wrapper'>
-					<p class='content__subheader'>{props.subheader}</p>
 					<h1 class='content__header'>{props.header}</h1>
-					{/* Navigation Links */}
-					<nav class='navigation'>
-						<Link to='/'>Home</Link>
-						<Link to='/projects'>Projects</Link>
-						<Link to='/blog'>Blog</Link>
-					</nav>
+					<p class='content__subheader'>{props.subheader}</p>
 
 					<div class='content__inner'>
 						{props.children}
