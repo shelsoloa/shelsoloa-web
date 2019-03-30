8d9a841e8088f4877051b6376ab771c8e6dbbe73
Shel Soloa <a.sheldon.sol@gmail.com>
2019-03-30 11:33:10 -0400

    Added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+
+export default () => (
+	<Layout header='Woops' subheader='Looks like we ran into an error'>
+		<br/>
+		<h1>404 - Page Not Found</h1>
+		<p>&nbsp;&nbsp;The page you requested could not be found. If you think this is a technical error then message me at <a href='mailto:contact@shelsoloa'>contact@shelsoloa.com</a>, and I'll have this fixed as soon as possible.<br/><br/>Thanks!<br/>&nbsp;&nbsp;~ Shel</p>
+	</Layout>
+)
