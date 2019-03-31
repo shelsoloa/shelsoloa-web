aea343d83e5b893a9393a432fb04543c444a84ec
Shel Soloa <a.sheldon.sol@gmail.com>
2019-03-31 13:04:33 -0400

    First draft of index page: Added heading and projects layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,35 @@ import '../styles/index.scss'
 
 
 export default ({data}) => (
-	<Layout class='index-page' header='ShelSoloa' subheader='~'>
-		<p>Homepage</p>
-		<h1 href='http://twitter.com/_shellbot_'>&#x2B22;</h1>
+	<Layout class='index-page' header='' subheader=''>
+		<img class='logo' alt="It's Shel Soloa" description='My personal logo'/>
+		<h1 class='wordmark'>ShelSoloa</h1>
+		<blockquote>Booking is currently CLOSED<br/>Availability opening in May</blockquote>
+
+		<p>This is the development journal of Sheldon Soloa.<br/>Here you can get an overview of my projects, writing, and the occasional update.</p>
+
+		<hr/>
+
+		<h2>Featured Projects</h2>
+		<div class='featured-projects'>
+			<div class='preview'>
+				<img class='preview__image' alt='gamedevinspo logo'/>
+				<h4 class='preview__title'>GameDevInspo</h4>
+				<hr/>
+				<p class='preview__description'>GDI is a content curation blog ran by shel.</p>
+			</div>
+			<div class='preview'>
+				<img class='preview__image' alt='gamedevinspo logo'/>
+				<h4 class='preview__title'>GameDevInspo</h4>
+				<hr/>
+				<p class='preview__description'>GDI is a content curation blog ran by shel.</p>
+			</div>
+			<div class='preview'>
+				<img class='preview__image' alt='gamedevinspo logo'/>
+				<h4 class='preview__title'>GameDevInspo</h4>
+				<hr/>
+				<p class='preview__description'>GDI is a content curation blog ran by shel.</p>
+			</div>
+		</div>
 	</Layout>
 )
