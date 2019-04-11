ecca939ac18c4b39c6ed35aaad45bc9db4905a75
Shel Soloa <shel@shelsoloa.com>
2019-04-11 18:51:56 -0400

    Added about page

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+
+export default () => (
+	<Layout class='about-page' header='About' subheader='thanks for your interest!'>
+
+		<p>Yo! If you're actually reading this, that means you've compiled this from source. Good work! Just know that this section is WIP and I literally wrote this all out in a few minutes. Don't judge me please. Thanks. Bye &lt;3</p>
+		
+		<h1>Me</h1>
+		<p>I'm Shel Soloa a developer from Canada. I learned to program rougly a decade ago, and ive spent so much time doing it that it has become a character trait of mine. Previously went by shellbot and before that pixhead so if you see anything refering to that on the web it might be me but also probably not.I refer to myself and a game / web developer because I spend a large amount of my time splt betweem tje twp. O camt decode pm a fabpirote! I also enjouy ddabbling in social media progrtooling and have a desire to eventually land inside machine learning . AI , ny favourite past time is programming and I want to get better at my craft whilst sharing everything I learn aong the way. This new venture of :social: programming is new to me, and I'm sure to have a lot to learn, but I hope you stick along for the ride! Thanks :D</p>
+
+		<h1>About the site</h1>
+		<p>I programmed this site as being kind of a central hub for myself, however knowing the type of person that i am, this site will go through constant revision and reworking. Currently its devekioed using Gatsby... but graphy ql, but jelyll looks pretty nice too. If you want to see what it is im working on you can follow me on twitter or drop me a line at any time, I love hearing from people! Seriously talk to me, programming is too sedentary,</p>
+
+		<h1>Links</h1>
+		<p>Below are links for basically everywhere that you can find me... these arent all programming but a majority of them art, ive marked which is which and what I intend to post there. You can find general news rambling pdates and my day to day on twitter, you can follow my projects on github you can follow my artistic work on tumblr, gamedev and oixel art, you can find my long format content of youtube where I talk about what I think is dope, and you can find a combination of the latter on medium. I hope to be able to do more in the future but... Bababy steps right</p>
+
+	</Layout>
+)
