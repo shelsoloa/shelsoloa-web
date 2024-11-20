04f9ab0fcd28df2d78e9d690b5f38f6ee0bc60b0
Shel Soloa <shel@shelsoloa.com>
2024-11-19 22:57:28 -0500

    Fix quotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,18 +11,18 @@ export default function Home() {
         <h1 className="text-4xl font-bold">Sheldon Soloa</h1>
         <p>Software Engineer based in Toronto, Canada</p>
 
-        <h2 className="text-lg font-bold mt-4">What I'm working on currently:</h2>
+        <h2 className="text-lg font-bold mt-4">What I&apos;m working on currently:</h2>
         <ul className="list-disc list-inside">
           <li>Founder @ <a href="https://promoexchange.io" className="text-blue-500">Promoexchange.io</a></li>
           <li>Fractional CTO @ Gabzebo Interactive Media</li>
           <li>Cloud Engineering Lead @ OtO Lawn</li>
         </ul>
 
-        <h2 className="text-lg font-bold mt-4">Things I've done in the past</h2>
+        <h2 className="text-lg font-bold mt-4">Things I&apos;ve done in the past</h2>
         <ul className="list-disc list-inside">
           <li>Yelp - I was a Senior Software Engineer in the Restaurants organization. I led a team of 15+ engineers to develop Yelp Guest Manager.</li>
           <li>GamedevInspo - Created a game development inspiration blog. Grew to roughly 10k followers across all platforms.</li>
-          <li>Game development under "shellbot" alias - Started a game development studio, ultimately bombed.</li>
+          <li>Game development under &ldquo;shellbot&rdquo; alias - Started a game development studio, ultimately bombed.</li>
         </ul>
 
         <h2 className="text-lg font-bold mt-4">Where to find me:</h2>
