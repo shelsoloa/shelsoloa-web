36ea53086ef8dbd47a3440c6a3d3df2552cb1158
Shel Soloa <shel@shelsoloa.com>
2024-11-24 20:34:33 -0500

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,28 +6,26 @@ export default function Home() {
     <div className="flex flex-col h-screen">
       <div className="flex flex-col max-w-5xl w-full mx-auto border p-8 my-8 rounded-lg">
         
-        <Image src={shellbot_logo} className="mx-auto" alt="shel soloa logo" width={100} height={100} />
+        <Image src={shellbot_logo} className="mx-auto mb-4" alt="shel soloa logo" width={100} height={100} />
         
         <h1 className="text-4xl font-bold">Sheldon Soloa</h1>
         <p>Software Engineer based in Toronto, Canada</p>
 
+        <div className="border flex flex-col border-blue-500 rounded-lg p-4 mt-4">
+          <p>Currently focusing on marketing tech. If you&apos;re in the influencer marketing space, <a href="mailto:shel@shelsoloa.com" className="text-blue-500">drop me a line!</a></p>
+        </div>
+
         <h2 className="text-lg font-bold mt-4">What I&apos;m working on currently:</h2>
         <ul className="list-disc list-inside">
           <li>Founder @ <a href="https://promoexchange.io" className="text-blue-500">Promoexchange.io</a></li>
-          <li>Fractional CTO @ Gabzebo Interactive Media</li>
           <li>Cloud Engineering Lead @ OtO Lawn</li>
-        </ul>
-
-        <h2 className="text-lg font-bold mt-4">Things I&apos;ve done in the past</h2>
-        <ul className="list-disc list-inside">
-          <li>Yelp - I was a Senior Software Engineer in the Restaurants organization. I led a team of 15+ engineers to develop Yelp Guest Manager.</li>
-          <li>GamedevInspo - Created a game development inspiration blog. Grew to roughly 10k followers across all platforms.</li>
-          <li>Game development under &ldquo;shellbot&rdquo; alias - Started a game development studio, ultimately bombed.</li>
+          <li>Fractional CTO @ Gabzebo Interactive Media</li>
         </ul>
 
         <h2 className="text-lg font-bold mt-4">Where to find me:</h2>
         <ul className="list-disc list-inside">
           <li>Email: <a href="mailto:shel@shelsoloa.com" className="text-blue-500">shel@shelsoloa.com</a></li>
+          <li>LinkedIn: <a href="https://www.linkedin.com/in/shelsoloa/" className="text-blue-500">Sheldon Soloa</a></li>
           <li>Twitter: <a href="https://twitter.com/shelsoloa" className="text-blue-500">@shelsoloa</a></li>
           <li>GitHub: <a href="https://github.com/shelsoloa" className="text-blue-500">shelsoloa</a></li>
         </ul>
