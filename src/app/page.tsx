71dcb9eedd821642e581e92320f51933e9ad3b0a
Shel Soloa <shel@shelsoloa.com>
2025-07-21 02:10:46 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,15 @@ export default function Home() {
         <h1 className="text-4xl font-bold">Sheldon Soloa</h1>
         <p>Software Engineer based in Toronto, Canada</p>
 
-        <div className="border flex flex-col border-blue-500 rounded-lg p-4 mt-4">
-          <p>Currently building digital marketing tech. If you&apos;re in the influencer marketing space, <a href="mailto:shel@shelsoloa.com" className="text-blue-500">drop me a line!</a></p>
-        </div>
-
         <h2 className="text-lg font-bold mt-4">What I&apos;m working on currently:</h2>
         <ul className="list-disc list-inside">
           <li>Founder @ <a href="https://branebridge.com" className="text-blue-500">BraneBridge</a></li>
-          <li>Founder @ <a href="https://promoexchange.io" className="text-blue-500">Promoexchange.io</a></li>
           <li>Cloud Engineering Lead @ OtO Lawn</li>
+        </ul>
+
+        <h2 className="text-lg font-bold mt-4">Other projects:</h2>
+        <ul className="list-disc list-inside">
+          <li>Founded @ <a href="https://promoexchange.io" className="text-blue-500">promoexchange.io</a></li>
         </ul>
 
         <h2 className="text-lg font-bold mt-4">Where to find me:</h2>
